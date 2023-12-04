2b6bbb1f661bfdb70630cbd18c7940d809e4a4c1
rootnure <nur.diu.2791@gmail.com>
2023-12-04 20:11:50 +0600

    stripe added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SK);
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -282,6 +283,23 @@ async function run() {
         bookingCount,
         deliveryCount: deliveryCount.length,
         userCount: userCount.length,
+      });
+    });
+
+    /* payment */
+    // payment intent
+    app.post("/create-payment-intent", async (req, res) => {
+      const { amount } = req.body;
+      const price = parseInt(amount * 100);
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: price,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret,
       });
     });
 
